1e735bda9da9e1c5a01400a39558e412e733dbb7
OrionMat <orionmathews@gmail.com>
2020-10-26 22:05:37 +0000

    added a search icon styled the seach bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,67 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as SearchIconSVG } from "./images/searchIcon.svg";
 
 const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 750px
-`
+  height: 750px;
+`;
+
+const SearchWrapper = styled.div`
+  position: relative;
+  width: 500px;
+  margin: auto;
+  align-items: center;
+`;
 
 const SearchBar = styled.input`
+  height: 50px;
+  width: 100%;
   border: 1px solid #dfe1e5;
-  width: 500px;
   border-radius: 25px;
   z-index: 3;
-  height: 50px;
-  margin: auto;
-
+  padding-left: 60px;
   :hover {
-    box-shadow: 0 1px 6px rgba(32,33,36,.28);
-    border-color: rgba(223,225,229,0);
+    box-shadow: 0 1px 6px rgba(32, 33, 36, 0.28);
+    border-color: rgba(223, 225, 229, 0);
   }
-`
+  :focus {
+    outline: none;
+    box-shadow: 0 1px 6px rgba(32, 33, 36, 0.28);
+    border-color: rgba(223, 225, 229, 0);
+  }
+  ::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+  }
+`;
+
+const SearchIcon = styled(SearchIconSVG)`
+  position: absolute;
+  margin-left: 10px;
+  width: 40px;
+  height: 100%;
+`;
+
+const ButtonsWrapper = styled.div``;
 
 const App = () => {
   return (
     <ContentWrapper id="content">
-      <SearchBar id="input" type="search" autoComplete="off" spellCheck="false" placeholder="Check a fact or statement" />
+      <SearchWrapper>
+        <SearchIcon />
+        <SearchBar
+          id="input"
+          type="search"
+          autoComplete="off"
+          spellCheck="false"
+          placeholder="Check a fact or statement"
+        />
+      </SearchWrapper>
+      <ButtonsWrapper>{}</ButtonsWrapper>
     </ContentWrapper>
   );
-}
+};
 
 export default App;
