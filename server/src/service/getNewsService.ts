8ba2b45ceccdda908f603a5e8fd2a16998f32eb2
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-12-24 16:53:10 +0000

    traced through link cleaning and mapping service

--- server/src/service/getNewsService.ts
+++ server/src/service/getNewsService.ts
@@ -2,55 +2,26 @@
 import { error } from "console";
 import { googleSearchIntegration } from "../integration/googleSearchIntegration";
 import { scrapePageHtml } from "../integration/scrapePageHtml";
-import { mapRawPages, NewsPiece } from "./mappingService";
+import { cleanLinks } from "./cleaningService";
+import { mappingService } from "./mappingService";
 
-export const getNewsService = async (requestBody: {
-  statement: string;
-  sources: string[];
-}) => {
-  const statement = requestBody.statement;
-  const sources = requestBody.sources;
+export const getNewsService = async (statement: string, sources: string[]) => {
   console.log(
     `scrapeNewsPieceService for sources "${sources}" with statement: "${statement}"`
   );
 
   // search internet for statement
   const queries = sources.map((source) => `${source} + ${statement}`);
-  const linksArrays = [
-    [
-      "https://www.bbc.co.uk/news/world-us-canada-55193939",
-      "https://www.bbc.co.uk/news/uk-politics-55191436",
-    ],
-    [
-      "https://uk.reuters.com/article/health-coronavirus-snapshot/what-you-need-to-know-about-the-coronavirus-right-now-idUKVIRUS1",
-      "https://uk.reuters.com/article/uk-britain-sterling/sterling-drops-from-two-and-a-half-year-high-as-brexit-talks-paused-idUKKBN28E134",
-    ],
-    [
-      "https://www.nytimes.com/2020/11/15/us/politics/trump-concession-books-literature-.html",
-      "https://www.nytimes.com/2020/11/07/us/politics/joe-biden-president.html",
-    ],
-    [
-      "https://apnews.com/article/coronavirus-aid-bill-congress-08974a416131377d846d7b5b8583d250",
-    ],
-  ]; // await googleSearchIntegration(queries);
+  const rawLinksArrays = await googleSearchIntegration(queries);
 
-  // TODO: clean links, limit them
-  // const linksArrays = cleanLinks(sources, rawLinksArrays)
+  // clean links, limit them
+  const linksArrays = await cleanLinks(sources, rawLinksArrays);
 
   // scrape related web pages
   const rawPageArrays = await scrapePageHtml(linksArrays);
-  if (!rawPageArrays) throw new Error("No html pages have been scraped");
 
   // parse html web pages
-  let newsPieces: NewsPiece[] = [];
-  for (let i = 0, end = sources.length; i < end; i++) {
-    const newsPieceArray = await mapRawPages(
-      sources[i],
-      linksArrays[i],
-      rawPageArrays[i]
-    );
-    newsPieces = newsPieces.concat(newsPieceArray);
-  }
+  let newsPieces = await mappingService(sources, linksArrays, rawPageArrays);
 
   return newsPieces;
 };
