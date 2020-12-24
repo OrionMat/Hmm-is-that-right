c49f32ac4a532859c73ded4c89c77a692b6415cc
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-12-24 16:47:16 +0000

    added link cleaning

---
+++ server/src/service/cleaningService.ts
@@ -0,0 +1,41 @@
+const filterLinks = async (source: string, sourceRawLinks: string[]) => {
+  let filterKey = "";
+  switch (source.toLowerCase()) {
+    case "bbc":
+      filterKey = "https://www.bbc.";
+      break;
+    case "nyt":
+      filterKey = "https://www.nytimes.";
+      break;
+    case "ap":
+      filterKey = "https://apnews.";
+      break;
+    case "reuters":
+      filterKey = "https://www.reuters.";
+      break;
+    default:
+      console.log("filterLinks Error: no source match");
+      break;
+  }
+
+  const links = sourceRawLinks.filter((rawLink) => rawLink.includes(filterKey));
+  return links;
+};
+
+export const cleanLinks = async (
+  sources: string[],
+  rawLinks: string[][]
+): Promise<string[][]> => {
+  let cleanLinks: string[][] = [];
+  for (
+    let sourceIndex = 0, sourceNum = sources.length;
+    sourceIndex < sourceNum;
+    sourceIndex++
+  ) {
+    const source = sources[sourceIndex];
+    const sourceRawLinks = rawLinks[sourceIndex];
+    const sourceLinks = await filterLinks(source, sourceRawLinks);
+    cleanLinks.push(sourceLinks);
+  }
+  return cleanLinks;
+};
