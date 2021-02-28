695fcfbd60c0f1fced1a4a258100e35c2e40ab22
OrionMat <orionmathews@gmail.com>
2021-02-28 18:31:20 +0000

    added TODO comments

--- server/src/service/getNewsPieces.ts
+++ server/src/service/getNewsPieces.ts
@@ -4,6 +4,10 @@ import { scrapePageHtml } from "../integration/scrapePageHtml";
 import { cleanUrls } from "./cleanUrls";
 import { parseHtml } from "./parseHtml";
 
+// TODO test throughy. Add unit test for each function
+// TODO make immutable data flow. Currently have to many objects referencing the same locations in memory
+// TODO add extraction of most similar sentences/paragraphs
+
 /**
  * Gets news pieces that can be used to cross check a statement.
  * @param statement Statement to be fact checked
@@ -24,7 +28,7 @@ export const getNewsPieces = async (statement: string, sources: string[]) => {
   const sourcePages = await scrapePageHtml(sourceUrls);
 
   // parse html web pages
-  let sourcePieces = await parseHtml(sourcePages);
+  const sourcePieces = await parseHtml(sourcePages);
 
   return sourcePieces;
 };
