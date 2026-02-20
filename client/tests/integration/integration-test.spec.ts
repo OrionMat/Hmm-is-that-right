d8c0aa899afa330b42e512189c0f2ed5244e991b
orion mathews <orionmathews@gmail.com>
2026-02-20 20:42:33 +0000

    added api mock for initial integration testing

--- client/tests/integration/integration-test.spec.ts
+++ client/tests/integration/integration-test.spec.ts
@@ -1,19 +1,42 @@
 import { test, expect } from "@playwright/test";
+import { mockGetNewsPiecesData } from "./mocks/apiResponses/getNewsPeieces";
 
 test("test", async ({ page }) => {
+  // Mocks
+  await page.route("**/getNewsPieces?*", async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify(mockGetNewsPiecesData),
+    });
+  });
+
+  // Actions
   await page.goto("/");
-
   await page.getByRole("link", { name: "Fact Check" }).click();
-
   await page
     .getByRole("searchbox", { name: "Check a fact or statement" })
     .click();
   await page
     .getByRole("searchbox", { name: "Check a fact or statement" })
-    .fill("Hello");
-  await page.getByRole("button").first().click();
+    .fill("Test statement");
+  await page.keyboard.press("Enter");
 
+  // Assertions
   await expect(
     page.getByRole("searchbox", { name: "Check a fact or statement" }),
-  ).toHaveValue("Hello");
+  ).toHaveValue("Test statement");
+  await expect(
+    page.getByRole("link", { name: "BBC News Title" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "NYT News Title" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "\nAP News Title\n" }),
+  ).toBeVisible();
+  await expect(
+    page.getByText("News sentence 1. News sentence 2."),
+  ).toBeVisible();
+  await expect(page.getByText("News sentence 3.")).toBeVisible();
 });
---
+++ client/tests/integration/mocks/apiResponses/getNewsPeieces.ts
@@ -0,0 +1,23 @@
+export const mockGetNewsPiecesData = [
+  {
+    url: "https://www.bbc.com/news/topics/cp7r8bgl6lgt",
+    title: "BBC News Title",
+    date: null,
+    body: ["News sentence 1.", "News sentence 2."],
+    source: "bbc",
+  },
+  {
+    url: "https://www.nytimes.com/artile/us/politics/100000010640150/news-artile.html",
+    title: "NYT News Title",
+    date: null,
+    body: ["News sentence 3."],
+    source: "nyt",
+  },
+  {
+    url: "https://apnews.com/hub/news-artile",
+    title: "\nAP News Title\n",
+    date: null,
+    body: [],
+    source: "ap",
+  },
+];
